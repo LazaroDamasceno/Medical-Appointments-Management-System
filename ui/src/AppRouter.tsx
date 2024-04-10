50367c9562ac515b68547955f1986e411b0dcf3a
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-10 01:23:53 -0300

    backend: created the AppRouter component

---
+++ ui/src/AppRouter.tsx
@@ -0,0 +1,19 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from 'react-router-dom'
+
+export const AppRouter = () => {
+
+    return (
+        <>
+          <BrowserRouter>
+            <Routes>
+                
+            </Routes>
+          </BrowserRouter>  
+        </>
+    )
+
+}

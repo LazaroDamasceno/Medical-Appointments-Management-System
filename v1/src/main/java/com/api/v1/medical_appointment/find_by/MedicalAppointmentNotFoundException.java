10d44c8d7644cf51864a44bf4b784b40543ff609
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-14 23:48:36 -0300

    Changed MedicalAppointmentNotFoundException

--- v1/src/main/java/com/api/v1/medical_appointment/find_by/MedicalAppointmentNotFoundException.java
+++ v1/src/main/java/com/api/v1/medical_appointment/find_by/MedicalAppointmentNotFoundException.java
@@ -1,28 +1,9 @@
 package com.api.v1.medical_appointment.find_by;
 
-import java.time.LocalDateTime;
-
-import com.api.v1.patient.Patient;
-import com.api.v1.physician.Physician;
-
 public class MedicalAppointmentNotFoundException extends RuntimeException {
 
-    public MedicalAppointmentNotFoundException(Patient patient, Physician physician, LocalDateTime dateTime) {
-        super("""
-            Medical appointment whose patient's SSN is %s, physician's MLN iss %s and scheduled date is %s was not found.
-        """.formatted(patient.getSystemUser().getSsn(), physician.getPhysicanLicenseNumber(), dateTime));
-    }
-
-    public MedicalAppointmentNotFoundException(Patient patient, LocalDateTime dateTime) {
-        super("""
-            Medical appointment whose patient's SSN is %s and scheduled date is %s was not found.
-        """.formatted(patient.getSystemUser().getSsn(), dateTime));
-    }
-
-    public MedicalAppointmentNotFoundException(Physician physician, LocalDateTime dateTime) {
-        super("""
-            Medical appointment whose physician's MLN is %s and scheduled date is %s was not found.
-        """.formatted(physician.getPhysicanLicenseNumber(), dateTime));
+    public MedicalAppointmentNotFoundException() {
+        super("Wanted medical appointment was not found.");
     }
     
 }

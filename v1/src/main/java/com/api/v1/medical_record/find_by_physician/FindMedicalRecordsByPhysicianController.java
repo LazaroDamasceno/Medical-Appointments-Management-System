f02be4303565cc62d26d8e476bfa2657372fb952
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-15 19:01:16 -0300

    Refactored the method  findByPhysicianLicenseNumber

--- v1/src/main/java/com/api/v1/medical_record/find_by_physician/FindMedicalRecordsByPhysicianController.java
+++ v1/src/main/java/com/api/v1/medical_record/find_by_physician/FindMedicalRecordsByPhysicianController.java
@@ -3,29 +3,14 @@ package com.api.v1.medical_record.find_by_physician;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.api.v1.medical_record.MedicalRecord;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping("api/v1/medical-record-by-physician")
-@RequiredArgsConstructor
-public class FindMedicalRecordsByPhysicianController implements FindMedicalRecordsByPhysician {
+public interface FindMedicalRecordsByPhysicianController {
     
-    private final FindMedicalRecordsByPhysicianService service;
-
-    @Override
-    @GetMapping("{physicianLicenseNumber}")
-    @Transactional(readOnly = true)
-    public ResponseEntity<List<MedicalRecord>> findByPhysicianLicenseNumber(@NotNull @Size(min=7, max=7) String physicianLicenseNumber) {
-        return service.findByPhysicianLicenseNumber(physicianLicenseNumber);
-    }
+    ResponseEntity<List<MedicalRecord>> findByPhysicianLicenseNumber(@NotNull @Size(min=7, max=7) String physicianLicenseNumber);
 
 }
---
+++ v1/src/main/java/com/api/v1/medical_record/find_by_physician/FindMedicalRecordsByPhysicianServiceImpl.java
@@ -0,0 +1,37 @@
+package com.api.v1.medical_record.find_by_physician;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.v1.medical_record.MedicalRecord;
+import com.api.v1.medical_record.MedicalRecordRepository;
+import com.api.v1.physician.Physician;
+import com.api.v1.physician.internal_use.FindPhysicianByLicenseNumber;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FindMedicalRecordsByPhysicianServiceImpl implements FindMedicalRecordsByPhysicianService {
+
+    private final MedicalRecordRepository repository;
+    private final FindPhysicianByLicenseNumber findPhysicianByLicenseNumber;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<MedicalRecord> findByPhysicianLicenseNumber(@NotNull @Size(min=7, max=7) String physicianLicenseNumber) {
+        Physician physician = findPhysicianByLicenseNumber.findByPhysicanLicenseNumber(physicianLicenseNumber);
+        List<MedicalRecord> medicalRecords = repository.findAll();
+        if (medicalRecords.isEmpty()) throw new NoMedicalRecordFoundException(physicianLicenseNumber);
+        return repository
+                .findAll()
+                .stream()
+                .filter(e -> e.getPhysician().equals(physician))
+                .toList();
+    }
+    
+}

37cfb30de9320a8d7b95aecfb9f8639c6ed6caa2
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-15 00:06:33 -0300

    Deleted the methods relared to find all medical appointments

--- v1/src/main/java/com/api/v1/medical_appointment/find_all/NoMedicalAppointmentFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.medical_appointment.find_all;
-
-public class NoMedicalAppointmentFoundException extends RuntimeException {
-
-    public NoMedicalAppointmentFoundException() {
-        super("No medical appointment was found.");
-    }
-    
-}

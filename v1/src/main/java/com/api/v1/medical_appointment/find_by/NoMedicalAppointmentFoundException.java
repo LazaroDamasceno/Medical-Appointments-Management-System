ab6aba701f240dd00c290868b0a7e238a6a5f8b3
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-17 03:17:16 -0300

    Deleted NoMedicalAppointmentFoundException

--- v1/src/main/java/com/api/v1/medical_appointment/find_by/NoMedicalAppointmentFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.medical_appointment.find_by;
-
-public class NoMedicalAppointmentFoundException extends RuntimeException {
-
-    public NoMedicalAppointmentFoundException() {
-        super("No medical appointment was found.");
-    }
-    
-}

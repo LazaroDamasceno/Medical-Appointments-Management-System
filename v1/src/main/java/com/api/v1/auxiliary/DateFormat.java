2566b1f49c0515e47d1a587b5d00f082060e1dbe
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-04-24 16:47:55 -0300

    Removed the annotation Size from DateFormat, DateTimeFormat and DateTimeFormatForGET

--- v1/src/main/java/com/api/v1/auxiliary/DateFormat.java
+++ v1/src/main/java/com/api/v1/auxiliary/DateFormat.java
@@ -7,12 +7,10 @@ import java.lang.annotation.Target;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 @NotNull
-@Size(min = 18, max = 18)
 @Pattern(regexp = "^([0-9]{2})/([0-9]{2})/([0-9]{4})$")
 public @interface DateFormat {
   String message() default "Invalid date time format. Please enter date time as dd/mm/yyyy hh:mm:ss.";
--- v1/src/main/java/com/api/v1/auxiliary/DateTimeFormatForGET.java
+++ v1/src/main/java/com/api/v1/auxiliary/DateTimeFormatForGET.java
@@ -7,12 +7,10 @@ import java.lang.annotation.Target;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 @NotNull
-@Size(min = 18, max = 18)
 @Pattern(regexp = "^([0-9]{2})-([0-9]{2})-([0-9]{4}) ([0-1][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$")
 public @interface DateTimeFormatForGET {
   String message() default "Invalid date time format. Please enter date time as dd/mm/yyyy hh:mm:ss.";

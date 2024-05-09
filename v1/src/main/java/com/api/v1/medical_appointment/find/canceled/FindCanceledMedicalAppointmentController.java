add93edd172e378baf25cc330c55c8727ad43e6f
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-05-08 22:25:51 -0300

    Updated FindCanceledMedicalAppointmentController

--- v1/src/main/java/com/api/v1/medical_appointment/find/canceled/FindCanceledMedicalAppointmentController.java
+++ v1/src/main/java/com/api/v1/medical_appointment/find/canceled/FindCanceledMedicalAppointmentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,13 @@ public class FindCanceledMedicalAppointmentController {
 
     @Transactional(readOnly = true)
     @GetMapping("by-patient/{ssn}")
-    List<MedicalAppointment> findByPatient(@SSN String ssn) {
+    List<MedicalAppointment> findByPatient(@SSN @PathVariable String ssn) {
         return service.findByPatient(ssn);
     }
 
     @Transactional(readOnly = true)
     @GetMapping("by-physician/{physicianLicenseNumber}")
-    List<MedicalAppointment> findByPhysician(@PhysicianLicenseNumber String physicianLicenseNumber) {
+    List<MedicalAppointment> findByPhysician(@PhysicianLicenseNumber @PathVariable String physicianLicenseNumber) {
         return service.findByPatient(physicianLicenseNumber);
     }
     
